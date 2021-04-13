3a6381d47bc1edbc5412439ae39687640ac106c4
Caleb Tutty <github@calebtutty.com>
2021-04-13 19:11:37 +0700

    feat: load model and score result

--- index.py
+++ index.py
@@ -1,4 +1,13 @@
-from flask import Flask
+from flask import Flask, request
+from joblib import load
+
+decision_tree = load('iris_classifier.joblib')
+
+IRIS_CLASS_NAMES = {
+  0: "Iris-Setosa",
+  1: "Iris-Versicolour",
+  2: "Iris-Virginica"
+}
 
 app = Flask(__name__)
 
@@ -8,9 +17,16 @@ PORT=5000
 def hello():
   return "Hello, World!"
 
-@app.route("/hi")
-def hello_again():
-  return "Hello, World 2!"
+@app.route("/score", methods=["POST"])
+def score_inputs():
+  content = request.json
+  val_to_score = content["values"]
+
+  result = decision_tree.predict([val_to_score])
+
+  iris_name_result = IRIS_CLASS_NAMES[result[0]]
+
+  return { "result": iris_name_result }
 
 if __name__ == '__main__':
   app.run(port=PORT)
